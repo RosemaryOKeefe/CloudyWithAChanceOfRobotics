56bd09a32858b1b91206833410afb6abc2351431
RishiGajera <78657383+RishiGajera@users.noreply.github.com>
2022-01-15 11:42:47 -0800

    Add files via upload

--- rishidebug_final.py
+++ rishidebug_final.py
@@ -1,3 +1,6 @@
+#####################################
+# 1/15/2022 : Updated the code to add database output, all file names are added in string
+####################################
 import re
 from re import M
 from typing import Dict
@@ -7,11 +10,12 @@ import pytesseract
 from pytesseract import pytesseract
 #import PIL
 #from PIL import Image
-#import csv
+import csv
 import PIL.Image
 from pytesseract import Output
 from re import search
 import numpy as np
+import os.path
 
 def count(x):
     length = len(x)
@@ -27,8 +31,10 @@ def count(x):
     print ("length,letters,numbers,space,other",x,length,letters,numbers,space,other)
     return length,letters,numbers,space,other
 
+#######################################
 ## Sub-function in python
 # Coverts to Grams
+#######################################
 def ret_val(temp_word):
     name_cmp_mg = 'mg'
     name_cmp_g = 'g'
@@ -52,19 +58,27 @@ def ret_val(temp_word):
     return(value_mg)
 
 
+#######################################
+# All file names and database names
+#######################################
+db_name = 'C:/Users/12086/Documents/python/data/Rishi_data1'
+img_name = 'data/readme.jpg'
 
+#######################################
 ## Reading out the image
 ## improve quality of image
 ## Need to make it dynamic based on image quality and size .. To be worked on ... 
-myconfig = r'--psm 11 --oem 3'
-img = cv2.imread('data/readme.jpg')
+#######################################
+myconfig = r'--psm 6 --oem 3'
 
+img = cv2.imread(img_name)
 img = cv2.resize(img, None, fx=1.7, fy=1.7, interpolation=cv2.INTER_CUBIC)
-img = cv2.cvtColor(img, 0)
-kernel = np.ones((1, 1), np.uint8)
-img = cv2.dilate(img, kernel, iterations=1)
-img = cv2.erode(img, kernel, iterations=1)
+#img = cv2.cvtColor(img, 0)
+#kernel = np.ones((1, 1), np.uint8)
+#img = cv2.dilate(img, kernel, iterations=1)
+#img = cv2.erode(img, kernel, iterations=1)
 
+# Code to print boxes in the Image
 height, width, _ = img.shape
 data = pytesseract.image_to_data(img, config=myconfig, output_type=Output.DICT)
 amount_boxes = len(data['text'])
@@ -74,14 +88,28 @@ for i in range(amount_boxes):
         #img = cv2.rectangle(img, (x,y), (x+width, y+height), (0, 255, 0), 2)
         #img = cv2.putText(img, data['text'][i], (x, y+height-20), cv2.FONT_HERSHEY_PLAIN, 0.7, (0, 255, 0), 1, cv2.LINE_AA)
 #cv2.imshow('img', img)
-#cv2.waitKey(0)
-text = pytesseract.image_to_string(img)
+
+
+###########
+# Covert img to text
+###########
+#gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#threshold = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+#threshold = 255 - threshold
+#threshold = cv2.resize(img, None, fx=1.7, fy=1.7, interpolation=cv2.INTER_CUBIC)
+#data = pytesseract.image_to_string(threshold,lang='eng', config='--psm 6')
+#print(data)
+
+text = pytesseract.image_to_string(img,lang='eng',config=myconfig)
 your_string = text
 list_of_words = your_string.split()
 #print (list_of_words)
 
+#######################################
 #Segmenting the label into areas of interest
 #Call subroutine as same function used all time
+# save the values
+#######################################
 next_word = list_of_words[list_of_words.index('Protein') + 1]
 protein_val = ret_val(next_word)
 next_word1 = list_of_words[list_of_words.index('Fat') + 1]
@@ -98,9 +126,25 @@ sodium_val = ret_val(next_word5)
 #Debug Statement
 # next_word_array = [next_word, next_word1, next_word2, next_word3, next_word4]
 
+#######################################
 ##Label out the values of each nutrients.
+#######################################
 print ("Prot:", next_word,":",protein_val, "Fat:", next_word1,":",Fat_val,"Carb:",next_word2,":", Carb_val,"Fiber:", next_word3,":",Fiber_val,"Sugar:", next_word4,":",Sugars_val,"Sodium:",next_word5,":",sodium_val )
-
+r_String = (protein_val, Fat_val,Carb_val,Fiber_val,Sugars_val,sodium_val )
 #Debug Statement
 #cv2.imshow('img', img)
 #cv2.waitKey(0)
+
+#######################################
+# Putting data from code into database
+#######################################
+# Add Header to the file name
+r_Header = ["Protien","Fat","Carb","Fiber","Sugar","Sodium"]
+if False == os.path.exists(db_name):
+   with open(db_name, 'w',encoding='UTF8',newline='') as f:
+      writer = csv.writer(f)  
+      writer.writerow(r_Header)
+#Append new data to database
+with open(db_name, 'a',encoding='UTF8',newline='') as f:
+   writer = csv.writer(f)
+   writer.writerow(r_String)
